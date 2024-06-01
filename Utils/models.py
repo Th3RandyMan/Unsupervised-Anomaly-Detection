be12626e4b59dd48abe127c93f0b0fb83c21a576
Randall Fowler <rlfowler@ucdavis.edu>
2024-06-01 12:46:36 -0700

    need to add training method and check if valid model

--- Utils/models.py
+++ Utils/models.py
@@ -24,27 +24,8 @@ class VAEmodel(nn.Module):
         self.input_dims = input_dims
         self.latent_dims = latent_dims
         self.build_model()
-        
 
-        # self.define_iterator()
-        # self.build_model()
-        # self.define_loss()
-        # self.training_variables()
-        # self.compute_gradients()
-        # self.init_saver()
-
-    # def define_iterator(self):
-    #     self.original_signal = torch.tensor([], dtype=torch.float32)
-    #     self.seed = torch.tensor([], dtype=torch.int64)
-    #     self.dataset = torch.utils.data.TensorDataset(self.original_signal)
-    #     self.dataset = torch.utils.data.random_split(self.dataset, [60000, 8000])[0]
-    #     self.dataset = torch.utils.data.DataLoader(self.dataset, batch_size=self.config['batch_size'], shuffle=True)
-    #     self.input_image = iter(self.dataset).next()
-    #     self.code_input = torch.tensor([], dtype=torch.float32)
-    #     self.is_code_input = torch.tensor([], dtype=torch.bool)
-    #     self.sigma2_offset = torch.tensor(self.config['sigma2_offset'])
-
-    def build_model(self, n_kernels:int = 512, n_channels:int = 1):
+    def build_model(self, n_kernels:int = 512, n_channels:int = 1, latent_dims:int = 6, sigma:float = 0.1, sigma2_offset:float = 1e-2):
         # init = nn.init.xavier_uniform_
 
         # Encoder Structure:
@@ -64,144 +45,149 @@ class VAEmodel(nn.Module):
         )
 
         # Latent Space:
-        self.code_mean = nn.Linear(self.config['code_size'] * 4, self.config['code_size'])
-        self.code_std_dev = nn.Linear(self.config['code_size'] * 4, self.config['code_size'])
-        self.code_std_dev.bias.data += 1e-2
+        # Could change the shape. Maybe add another linear layer before the mean and std_dev layers.
+        self.code_mean = nn.Linear(latent_dims * 4, latent_dims)
+        self.code_std_dev = nn.Linear(latent_dims * 4, latent_dims)
+        self.code_std_dev.bias.data += sigma2_offset
 
         # Decoder Structure:
         self.decoder = nn.Sequential(
-            nn.Linear(self.config['code_size'], self.config['num_hidden_units']),
+            nn.Linear(latent_dims, n_kernels),
             nn.LeakyReLU(),
-            nn.Conv2d(self.config['num_hidden_units'], self.config['num_hidden_units'], kernel_size=1, padding='same'),
+            nn.Conv2d(n_kernels, n_kernels, kernel_size=1, padding='same'),
             nn.LeakyReLU(),
-            nn.Conv2d(self.config['num_hidden_units'], self.config['num_hidden_units'] // 4,
-                      kernel_size=(3, 1), stride=1, padding='same'),
-            nn.LeakyReLU(),
-            nn.PixelShuffle(2),
-            nn.Conv2d(self.config['num_hidden_units'] // 4, self.config['num_hidden_units'] // 8,
-                      kernel_size=(3, 1), stride=1, padding='same'),
+            nn.Conv2d(n_kernels, n_kernels // 4,
+                      kernel_size=3, stride=1, padding='same'),
             nn.LeakyReLU(),
             nn.PixelShuffle(2),
-            nn.Conv2d(self.config['num_hidden_units'] // 8, self.config['num_hidden_units'] // 16,
-                      kernel_size=(3, 1), stride=1, padding='same'),
+            nn.Conv2d(n_kernels // 4, n_kernels // 8,
+                      kernel_size=3, stride=1, padding='same'),
             nn.LeakyReLU(),
             nn.PixelShuffle(2),
-            nn.Conv2d(self.config['num_hidden_units'] // 16, self.config['n_channel'],
-                      kernel_size=(9, 1), stride=1, padding='valid')
+            nn.Conv2d(n_kernels // 8, n_kernels // 16,
+                      kernel_size=3, stride=1, padding='same'),
+            nn.LeakyReLU(),
+            nn.PixelShuffle(2),
+            nn.Conv2d(n_kernels // 16, n_channels,
+                      kernel_size=9, stride=1, padding='valid')
         )
 
-        self.sigma2 = nn.Parameter(torch.tensor(self.config['sigma'])**2 + self.sigma2_offset)
+        self.sigma2 = nn.Parameter(torch.tensor(sigma)**2 + sigma2_offset)
 
     def forward(self, x):
-        encoded_signal = self.encoder(x)
+        encoded_signal = self.encoder(x)    # Encode the input signal into the latent space
         encoded_signal = encoded_signal.view(encoded_signal.size(0), -1)
-        code_mean = self.code_mean(encoded_signal)
-        code_std_dev = self.code_std_dev(encoded_signal)
-        code_std_dev = torch.relu(code_std_dev) + 1e-2
-        mvn = tdist.MultivariateNormal(code_mean, torch.diag_embed(code_std_dev))
-        code_sample = mvn.sample()
-        decoded = self.decoder(code_sample.unsqueeze(-1).unsqueeze(-1))
+
+        code_mean = self.code_mean(encoded_signal)  # Get the mean of the latent space
+        code_std_dev = self.code_std_dev(encoded_signal)    # Get the standard deviation of the latent space
+        code_std_dev = torch.relu(code_std_dev) + 1e-2      # Bias the std
+        mvn = tdist.MultivariateNormal(code_mean, torch.diag_embed(code_std_dev)) # Create a multivariate normal distribution
+        code_sample = mvn.sample()  # Sample from the distribution
+        decoded = self.decoder(code_sample.unsqueeze(-1).unsqueeze(-1)) # Decode the sample
         return code_mean, code_std_dev, code_sample, decoded
+
 
 
 class lstmKerasModel:
     def __init__(self, data):
         pass
 
-    def create_lstm_model(self, config):
+    def create_lstm_model(self, n_neurons: int = 64, code_size: int = 6):
         lstm_model = nn.Sequential(
-            nn.LSTM(config['code_size'], config['num_hidden_units_lstm'], batch_first=True, return_sequences=True),
-            nn.LSTM(config['num_hidden_units_lstm'], config['code_size'], batch_first=True, return_sequences=True)
+            nn.LSTM(code_size, n_neurons, batch_first=True, return_sequences=True),
+            nn.LSTM(n_neurons, n_neurons, batch_first=True, return_sequences=True),
+            nn.LSTM(n_neurons, code_size, batch_first=True, return_sequences=True),
+            nn.Linear(code_size, code_size)
         )
         return lstm_model
 
-    def produce_embeddings(self, config, model_vae, data, sess):
-        self.embedding_lstm_train = torch.zeros((data.n_train_lstm, config['l_seq'], config['code_size']))
-        for i in range(data.n_train_lstm):
-            self.embedding_lstm_train[i] = model_vae(data.train_set_lstm['data'][i])[2].detach().numpy()
-        print("Finish processing the embeddings of the entire dataset.")
-        print("The first a few embeddings are\n{}".format(self.embedding_lstm_train[0, 0:5]))
-        self.x_train = self.embedding_lstm_train[:, :config['l_seq'] - 1]
-        self.y_train = self.embedding_lstm_train[:, 1:]
+    # def produce_embeddings(self, config, model_vae, data, sess):
+    #     self.embedding_lstm_train = torch.zeros((data.n_train_lstm, config['l_seq'], config['code_size']))
+    #     for i in range(data.n_train_lstm):
+    #         self.embedding_lstm_train[i] = model_vae(data.train_set_lstm['data'][i])[2].detach().numpy()
+    #     print("Finish processing the embeddings of the entire dataset.")
+    #     print("The first a few embeddings are\n{}".format(self.embedding_lstm_train[0, 0:5]))
+    #     self.x_train = self.embedding_lstm_train[:, :config['l_seq'] - 1]
+    #     self.y_train = self.embedding_lstm_train[:, 1:]
 
-        self.embedding_lstm_test = torch.zeros((data.n_val_lstm, config['l_seq'], config['code_size']))
-        for i in range(data.n_val_lstm):
-            self.embedding_lstm_test[i] = model_vae(data.val_set_lstm['data'][i])[2].detach().numpy()
-        self.x_test = self.embedding_lstm_test[:, :config['l_seq'] - 1]
-        self.y_test = self.embedding_lstm_test[:, 1:]
+    #     self.embedding_lstm_test = torch.zeros((data.n_val_lstm, config['l_seq'], config['code_size']))
+    #     for i in range(data.n_val_lstm):
+    #         self.embedding_lstm_test[i] = model_vae(data.val_set_lstm['data'][i])[2].detach().numpy()
+    #     self.x_test = self.embedding_lstm_test[:, :config['l_seq'] - 1]
+    #     self.y_test = self.embedding_lstm_test[:, 1:]
 
-    def load_model(self, lstm_model, config, checkpoint_path):
-        if checkpoint_path.exists():
-            lstm_model.load_state_dict(torch.load(checkpoint_path))
-            print("LSTM model loaded.")
-        else:
-            print("No LSTM model loaded.")
+    # def load_model(self, lstm_model, config, checkpoint_path):
+    #     if checkpoint_path.exists():
+    #         lstm_model.load_state_dict(torch.load(checkpoint_path))
+    #         print("LSTM model loaded.")
+    #     else:
+    #         print("No LSTM model loaded.")
 
-    def train(self, config, lstm_model, cp_callback):
-        optimizer = optim.Adam(lstm_model.parameters(), lr=config['learning_rate_lstm'])
-        criterion = nn.MSELoss()
-        for epoch in range(config['num_epochs_lstm']):
-            for x, y in zip(self.x_train, self.y_train):
-                optimizer.zero_grad()
-                output = lstm_model(x.unsqueeze(0))
-                loss = criterion(output, y.unsqueeze(0))
-                loss.backward()
-                optimizer.step()
-            print(f"Epoch {epoch+1}/{config['num_epochs_lstm']}, Loss: {loss.item()}")
-        torch.save(lstm_model.state_dict(), cp_callback)
+    # def train(self, config, lstm_model, cp_callback):
+    #     optimizer = optim.Adam(lstm_model.parameters(), lr=config['learning_rate_lstm'])
+    #     criterion = nn.MSELoss()
+    #     for epoch in range(config['num_epochs_lstm']):
+    #         for x, y in zip(self.x_train, self.y_train):
+    #             optimizer.zero_grad()
+    #             output = lstm_model(x.unsqueeze(0))
+    #             loss = criterion(output, y.unsqueeze(0))
+    #             loss.backward()
+    #             optimizer.step()
+    #         print(f"Epoch {epoch+1}/{config['num_epochs_lstm']}, Loss: {loss.item()}")
+    #     torch.save(lstm_model.state_dict(), cp_callback)
 
-    def plot_reconstructed_lt_seq(self, idx_test, config, model_vae, sess, data, lstm_embedding_test):
-        decoded_seq_vae = model_vae(data.val_set_lstm['data'][idx_test])[3].detach().numpy().squeeze()
-        print("Decoded seq from VAE: {}".format(decoded_seq_vae.shape))
+    # def plot_reconstructed_lt_seq(self, idx_test, config, model_vae, sess, data, lstm_embedding_test):
+    #     decoded_seq_vae = model_vae(data.val_set_lstm['data'][idx_test])[3].detach().numpy().squeeze()
+    #     print("Decoded seq from VAE: {}".format(decoded_seq_vae.shape))
 
-        decoded_seq_lstm = lstm_model(self.embedding_lstm_test[idx_test].unsqueeze(0)).detach().numpy().squeeze()
-        print("Decoded seq from lstm: {}".format(decoded_seq_lstm.shape))
+    #     decoded_seq_lstm = lstm_model(self.embedding_lstm_test[idx_test].unsqueeze(0)).detach().numpy().squeeze()
+    #     print("Decoded seq from lstm: {}".format(decoded_seq_lstm.shape))
 
-        fig, axs = plt.subplots(config['n_channel'], 2, figsize=(15, 4.5 * config['n_channel']), edgecolor='k')
-        fig.subplots_adjust(hspace=.4, wspace=.4)
-        axs = axs.ravel()
-        for j in range(config['n_channel']):
-            for i in range(2):
-                axs[i + j * 2].plot(np.arange(0, config['l_seq'] * config['l_win']),
-                                    np.reshape(data.val_set_lstm['data'][idx_test, :, :, j],
-                                               (config['l_seq'] * config['l_win'])))
-                axs[i + j * 2].grid(True)
-                axs[i + j * 2].set_xlim(0, config['l_seq'] * config['l_win'])
-                axs[i + j * 2].set_xlabel('samples')
-            if config['n_channel'] == 1:
-                axs[0 + j * 2].plot(np.arange(0, config['l_seq'] * config['l_win']),
-                                    np.reshape(decoded_seq_vae, (config['l_seq'] * config['l_win'])), 'r--')
-                axs[1 + j * 2].plot(np.arange(config['l_win'], config['l_seq'] * config['l_win']),
-                                    np.reshape(decoded_seq_lstm, ((config['l_seq'] - 1) * config['l_win'])), 'g--')
-            else:
-                axs[0 + j * 2].plot(np.arange(0, config['l_seq'] * config['l_win']),
-                                    np.reshape(decoded_seq_vae[:, :, j], (config['l_seq'] * config['l_win'])), 'r--')
-                axs[1 + j * 2].plot(np.arange(config['l_win'], config['l_seq'] * config['l_win']),
-                                    np.reshape(decoded_seq_lstm[:, :, j], ((config['l_seq'] - 1) * config['l_win'])), 'g--')
-            axs[0 + j * 2].set_title('VAE reconstruction - channel {}'.format(j))
-            axs[1 + j * 2].set_title('LSTM reconstruction - channel {}'.format(j))
-            for i in range(2):
-                axs[i + j * 2].legend(('ground truth', 'reconstruction'))
-            savefig(config['result_dir'] + "lstm_long_seq_recons_{}.pdf".format(idx_test))
-            fig.clf()
-            plt.close()
+    #     fig, axs = plt.subplots(config['n_channel'], 2, figsize=(15, 4.5 * config['n_channel']), edgecolor='k')
+    #     fig.subplots_adjust(hspace=.4, wspace=.4)
+    #     axs = axs.ravel()
+    #     for j in range(config['n_channel']):
+    #         for i in range(2):
+    #             axs[i + j * 2].plot(np.arange(0, config['l_seq'] * config['l_win']),
+    #                                 np.reshape(data.val_set_lstm['data'][idx_test, :, :, j],
+    #                                            (config['l_seq'] * config['l_win'])))
+    #             axs[i + j * 2].grid(True)
+    #             axs[i + j * 2].set_xlim(0, config['l_seq'] * config['l_win'])
+    #             axs[i + j * 2].set_xlabel('samples')
+    #         if config['n_channel'] == 1:
+    #             axs[0 + j * 2].plot(np.arange(0, config['l_seq'] * config['l_win']),
+    #                                 np.reshape(decoded_seq_vae, (config['l_seq'] * config['l_win'])), 'r--')
+    #             axs[1 + j * 2].plot(np.arange(config['l_win'], config['l_seq'] * config['l_win']),
+    #                                 np.reshape(decoded_seq_lstm, ((config['l_seq'] - 1) * config['l_win'])), 'g--')
+    #         else:
+    #             axs[0 + j * 2].plot(np.arange(0, config['l_seq'] * config['l_win']),
+    #                                 np.reshape(decoded_seq_vae[:, :, j], (config['l_seq'] * config['l_win'])), 'r--')
+    #             axs[1 + j * 2].plot(np.arange(config['l_win'], config['l_seq'] * config['l_win']),
+    #                                 np.reshape(decoded_seq_lstm[:, :, j], ((config['l_seq'] - 1) * config['l_win'])), 'g--')
+    #         axs[0 + j * 2].set_title('VAE reconstruction - channel {}'.format(j))
+    #         axs[1 + j * 2].set_title('LSTM reconstruction - channel {}'.format(j))
+    #         for i in range(2):
+    #             axs[i + j * 2].legend(('ground truth', 'reconstruction'))
+    #         savefig(config['result_dir'] + "lstm_long_seq_recons_{}.pdf".format(idx_test))
+    #         fig.clf()
+    #         plt.close()
 
-    def plot_lstm_embedding_prediction(self, idx_test, config, model_vae, sess, data, lstm_embedding_test):
-        self.plot_reconstructed_lt_seq(idx_test, config, model_vae, sess, data, lstm_embedding_test)
+    # def plot_lstm_embedding_prediction(self, idx_test, config, model_vae, sess, data, lstm_embedding_test):
+    #     self.plot_reconstructed_lt_seq(idx_test, config, model_vae, sess, data, lstm_embedding_test)
 
-        fig, axs = plt.subplots(2, config['code_size'] // 2, figsize=(15, 5.5), edgecolor='k')
-        fig.subplots_adjust(hspace=.4, wspace=.4)
-        axs = axs.ravel()
-        for i in range(config['code_size']):
-            axs[i].plot(np.arange(1, config['l_seq']), np.squeeze(self.embedding_lstm_test[idx_test, 1:, i]))
-            axs[i].plot(np.arange(1, config['l_seq']), np.squeeze(lstm_embedding_test[idx_test, :, i]))
-            axs[i].set_xlim(1, config['l_seq'] - 1)
-            axs[i].set_ylim(-2.5, 2.5)
-            axs[i].grid(True)
-            axs[i].set_title('Embedding dim {}'.format(i))
-            axs[i].set_xlabel('windows')
-            if i == config['code_size'] - 1:
-                axs[i].legend(('VAE\nembedding', 'LSTM\nembedding'))
-        savefig(config['result_dir'] + "lstm_seq_embedding_{}.pdf".format(idx_test))
-        fig.clf()
-        plt.close()
+    #     fig, axs = plt.subplots(2, config['code_size'] // 2, figsize=(15, 5.5), edgecolor='k')
+    #     fig.subplots_adjust(hspace=.4, wspace=.4)
+    #     axs = axs.ravel()
+    #     for i in range(config['code_size']):
+    #         axs[i].plot(np.arange(1, config['l_seq']), np.squeeze(self.embedding_lstm_test[idx_test, 1:, i]))
+    #         axs[i].plot(np.arange(1, config['l_seq']), np.squeeze(lstm_embedding_test[idx_test, :, i]))
+    #         axs[i].set_xlim(1, config['l_seq'] - 1)
+    #         axs[i].set_ylim(-2.5, 2.5)
+    #         axs[i].grid(True)
+    #         axs[i].set_title('Embedding dim {}'.format(i))
+    #         axs[i].set_xlabel('windows')
+    #         if i == config['code_size'] - 1:
+    #             axs[i].legend(('VAE\nembedding', 'LSTM\nembedding'))
+    #     savefig(config['result_dir'] + "lstm_seq_embedding_{}.pdf".format(idx_test))
+    #     fig.clf()
+    #     plt.close()
