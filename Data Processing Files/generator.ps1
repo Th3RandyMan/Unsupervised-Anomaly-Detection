3b27ecdf31dfe66d993bcec1c1273363d10c4486
cfking24 <77686459+cfking24@users.noreply.github.com>
2024-06-10 14:11:33 -0700

    creating the index files for ppg anomalies

--- Data Processing Files/generator.ps1
+++ Data Processing Files/generator.ps1
@@ -2,5 +2,5 @@ $numberOfRuns = 50
 
 for ($i = 1; $i -le $numberOfRuns; $i++) {
     Write-Host "Running iteration $i"
-    jupyter nbconvert --execute --to notebook --inplace dirty_ppgData_maker.ipynb
+    jupyter nbconvert --execute --to notebook --inplace dirty_ppgData_maker_improved.ipynb
 }
